656a707059253b6967ca1e9efb173a682f095c8c
arthay <arthur15251@gmail.com>
2021-09-04 22:33:02 +0400

    run

--- index.js
+++ index.js
@@ -2,22 +2,24 @@ const core = require('@actions/core');
 
 function run() {
   try {
-    const inputArray = JSON.parse(core.getInput('array'));
-    const json = core.getInput('json');
-
-    let value = JSON.parse(json);
-    let item1 = '';
-
-    for (const item of inputArray) {
-      item1 = item;
-      if (value[item]) {
-        value = value[item]
-      }
-    }
-
-    core.setOutput('value', JSON.stringify(value));
-    core.setOutput('array', JSON.stringify(inputArray));
+    const inputArray = core.getInput('array');
     core.setOutput('item', typeof inputArray);
+    // const inputArray = JSON.parse(core.getInput('array'));
+    // const json = core.getInput('json');
+    //
+    // let value = JSON.parse(json);
+    // let item1 = '';
+    //
+    // for (const item of inputArray) {
+    //   item1 = item;
+    //   if (value[item]) {
+    //     value = value[item]
+    //   }
+    // }
+    //
+    // core.setOutput('value', JSON.stringify(value));
+    // core.setOutput('array', JSON.stringify(inputArray));
+    // core.setOutput('item', typeof inputArray);
   } catch (err) {
     core.setFailed(err.message);
   }
