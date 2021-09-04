a188383959c1d6e9784873cc57590fd737c68be7
arthay <arthur15251@gmail.com>
2021-09-04 22:25:35 +0400

    run

--- dist/index.js
+++ dist/index.js
@@ -156,14 +156,18 @@ function run() {
     const json = core.getInput('json');
 
     let value = JSON.parse(json);
+    let item1 = '';
 
     for (const item of inputArray) {
+      item1 = item;
       if (value[item]) {
         value = value[item]
       }
     }
 
     core.setOutput('value', JSON.stringify(value));
+    core.setOutput('array', JSON.stringify(inputArray));
+    core.setOutput('item', JSON.stringify(item1));
   } catch (err) {
     core.setFailed(err.message);
   }
